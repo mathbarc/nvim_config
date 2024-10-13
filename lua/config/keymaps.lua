558c3be218de5ca2f5128508cd9ab6feb6255a90
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2024-10-13 16:58:41 -0300

    mapping keys for cmake commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,8 @@ map.del("n", "<leader>gG")
 map.del("n", "<leader>gl")
 map.del("n", "<leader>gL")
 map.del("n", "<leader>gf")
+
+map.set("n","<leader>mb", ":CMakeBuild<CR>")
+map.set("n","<leader>mg", ":CMakeGenerate<CR>")
+map.set("n","<leader>md", ":CMakeDebug<CR>")
+map.set("n","<leader>ms", ":CMakeSelectBuildType<CR>")
