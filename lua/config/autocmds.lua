7c9092d2cbb888024472a9598bb4471b0943662e
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2024-11-05 23:00:16 -0300

    finishing cmake configuration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local osys = require("cmake-tools.osys")
+require("cmake-tools").setup({
+    cmake_generate_options = { "-GNinja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+    cmake_build_directory = function()
+        if osys.iswin32 then
+            return "build\\${variant:buildType}"
+        end
+        return "build/${variant:buildType}"
+    end,
+})
