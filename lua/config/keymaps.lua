b0c12cc159643ec1724632f197de034410a8be27
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2024-11-10 19:06:19 -0300

    organizing keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,10 @@
 local map = vim.keymap
 
 map.set("n", "<leader>cc", "", { desc = "CMake Tools" })
-map.set("n", "<leader>ccm", "<cmd>CMakeBuild<CR>", { desc = "Build CMake Project" })
 map.set("n", "<leader>ccg", "<cmd>CMakeGenerate<CR>", { desc = "Generate CMake Build Configuration" })
-map.set("n", "<leader>ccd", "<cmd>CMakeDebug<CR>", { desc = "Debug CMake Project" })
 map.set("n", "<leader>ccs", "<cmd>CMakeSelectBuildType<CR>", { desc = "Select CMake Build Type" })
+map.set("n", "<leader>ccm", "<cmd>CMakeBuild<CR>", { desc = "Build CMake Project" })
 map.set("n", "<leader>cct", "<cmd>CMakeSelectLaunchTarget<CR>", { desc = "Select Target for execution" })
+map.set("n", "<leader>ccr", "<cmd>CMakeRun<CR>", { desc = "Run Target" })
+map.set("n", "<leader>ccd", "<cmd>CMakeDebug<CR>", { desc = "Debug CMake Project" })
 map.set("n", "<leader>cce", "<cmd>CMakeRunTest<CR>", { desc = "Run Tests" })
