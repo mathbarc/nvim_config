96fbb50de8ecf5b594980ad0b2aafeee6960549f
Matheus Barcelos de Oliveira <matheus.barcelos@solv.tec.br>
2026-04-16 04:30:13 -0300

    simplifying lspconfig

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,14 +1,1 @@
 vim.lsp.handlers["$/progress"] = function() end
-local lspconfig = require("lspconfig")
-
-lspconfig.ltex_plus.setup({
-    handlers = {
-        ["$/progress"] = function() end,
-    },
-})
-
-lspconfig.pyright.setup({
-    handlers = {
-        ["$/progress"] = function() end,
-    },
-})
